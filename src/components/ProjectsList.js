c0355c86bf0e703448d2a8a77d9dc7dc0d8fd9f5
Michael <michaelr9wilson@gmail.com>
2021-09-30 05:18:10 -0700

    styling additions

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -5,15 +5,15 @@ const ProjectsList = ()=>{
         <>
             <div className={"project_container"}>
                 <h3>UHDA</h3>
-                <div>
+
                     <p>
                         UHDA(Untitled Help Desk Application) is A Java Spring MVC Application that was created by myself, along with two of my classmates. This system is useful to IT
                         employees because users can upload tickets, while employees can monitor and update ticket states. Admins are given the permission to modify employee and user data, as
-                        well as assign tickets to individuals. 
+                        well as assign tickets to individuals.
                     </p>
-                </div>
+
                 <div className={"project_button_group"}>
-                    <a href={"https://github.com/jake-pauls/Untitled_UHDA_Project/wiki"}>
+                    <a className={"project_button"} href={"https://github.com/jake-pauls/Untitled_UHDA_Project/wiki"}>
                         <i style={{marginRight:5}} className="fab fa-github"/>
                         github
                     </a>
@@ -21,12 +21,12 @@ const ProjectsList = ()=>{
             </div>
             <div className={"project_container"}>
                 <h3>Mixology</h3>
-                <div>
+
                     <p>
                         Mobile application that runs on react-native. Choose from over 600 unique and classic cocktails with instructions on how to make.
                         Keep a list of you're favorites or even have our mood tester pick a drink for you. Happy hour is almost here.
                     </p>
-                </div>
+
                 <div className={"project_button_group"}>
                     <a href={"https://github.com/mikrowil/MixologyV2"}>
                         <i style={{marginRight:5}} className="fab fa-github"/>
@@ -36,13 +36,13 @@ const ProjectsList = ()=>{
             </div>
             <div className={"project_container"}>
                 <h3>Blockchain - Backend</h3>
-                <div>
+
                     <p>
                         The backend for a blockchain and crypto currency. Blockchains are an immutable and public database, tasked with keeping track of crypto transactions.
                         Server set up and class details are served here.
 
                     </p>
-                </div>
+
                 <div className={"project_button_group"}>
 
                         <a href={"https://github.com/mikrowil/block-chain-backend"}>
@@ -53,13 +53,13 @@ const ProjectsList = ()=>{
             </div>
             <div className={"project_container"}>
                 <h3>Restaurant Companion</h3>
-                <div>
+
                     <p>
                         A PHP app that displays employee data from an SQL database.
                     </p>
-                </div>
+
                 <div className={"project_button_group"}>
-                    <a href={"https://github.com/mikrowil/restaurant-companion"}>
+                    <a  href={"https://github.com/mikrowil/restaurant-companion"}>
                         <i style={{marginRight:5}} className="fab fa-github"/>
                         github
                     </a>
